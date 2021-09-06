57efe45b985321592de61d40c75ee42291a07ec4
idriska <idriscumali@gmail.com>
2021-09-06 17:23:37 +0300

    refactor card store logic

--- function/iyzico-functions-1.js
+++ function/iyzico-functions-1.js
@@ -8,64 +8,75 @@ HOW TO USE?
     2- In merchant settings, copy the API Key and Secret Key
     3- Set IYZIPAY_API_KEY, IYZIPAY_SECRET_KEY, IYZIPAY_URI values in Environment Variables
     4- Send a POST request to the endpoint https://<PROJECT DOMAIN>.hq.spicaengine.com/api/fn-execute/iyzipay
-    5- Body parameters should be like this
+        4.1- Body parameters should be like this
+            {
+                formType: 'popup', // If you want to use the Iyzico payment form (There are 2 types: popup, responsive)
+                data: {
+                    locale: 'TR', // not required (default TR),
+                    currency: 'TRY', // not required (default TRY),
+                    installment: '1', // not required (default 1 There are 6 types: 1, 2, 3, 6, 9, 12)
+                    // If you want to use the Iyzico payment form, do not include in "paymentCard" parameter
+                    // paymentCard: '61307f580d902e002c3ca185', // If using a saved card, set the _id from the IYZICO-Card Bucket
+                    paymentCard: {
+                        register_card: '0', // not required
+                        card_holder_name: 'Test User',
+                        card_number: '4603450000000000',
+                        expire_month: '12',
+                        expire_year: '25',
+                        cvc: '152',
+                        zip_code: '07000', // not required
+                    },
+                    // buyer: '61307fb00d902e002c3ca187', // If using a saved buyer, set the _id from the IYZICO-Buyer Bucket
+                    buyer: {
+                        id: 'BWDQ',
+                        name: 'Test',
+                        surname: 'User',
+                        email: 'test@gmail.com',
+                        identity_number: '231312123412',
+                        gsm_number: '333232432',
+                        ip: '192.168.1.1',
+                        city: 'Antalya',
+                        country: 'Turkey',
+                        registration_address: 'Arapsuyu mah. Konyaaltı',
+                        zipCode: '07000', // not required
+                    },
+                    // billingAddress: '612f70600d902e002c3ca0ca', // If using a saved billingAddress, set the _id from the IYZICO-Billing Address Bucket
+                    billingAddress: {
+                        city: 'Istanbul',
+                        country: 'Turkey',
+                        address: 'Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1',
+                        contact_name: 'Test User',
+                        zip_code: '34000', // not required
+                    },
+                    // shippingAddress is required if there is a PHYSICAL type product in the basket. Not required (for VIRTUAL type products)
+                    // shippingAddress: '61307f330d902e002c3ca183', // If using a saved shippingAddress, set the _id from the IYZICO-Shipping Address Bucket
+                    shippingAddress: {
+                        contact_name: 'Test User',
+                        city: 'Antalya',
+                        country: 'Turkey',
+                        address: 'Arapsuyu mah. Konyaaltı',
+                        zip_code: '07000', // not required
+                    },
+                    // basket: '61307ef20d902e002c3ca17f', // If using a saved basket, set the _id from the IYZICO-Basket Items Address Bucket
+                    basket: {
+                        name: 'Basket',
+                        product: ['612f8cc00d902e002c3ca0e3', '612f92200d902e002c3ca0f1'], // In the array, add the _id of the product that is in the IYZICO-Product Bucket
+                    },
+                    paidPrice: 160,
+                },
+            };
+    5- To create a paymnet card, send a POST request to the endpoint https://<PROJECT DOMAIN>.hq.spicaengine.com/api/fn-execute/inquirecard
+        5.1- Body parameters should be like this
         {
-            formType: 'popup', // If you want to use the Iyzico payment form (There are 2 types: popup, responsive)
-            data: {
-                locale: 'TR', // not required (default TR),
-                currency: 'TRY', // not required (default TRY),
-                installment: '1', // not required (default 1 There are 6 types: 1, 2, 3, 6, 9, 12)
-                // If you want to use the Iyzico payment form, do not include in "paymentCard" parameter
-                // paymentCard: '61307f580d902e002c3ca185', // If using a saved card, set the _id from the IYZICO-Payment Card Bucket
-                paymentCard: {
-                    register_card: '0', // not required
-                    card_holder_name: 'Test User',
-                    card_number: '4603450000000000',
-                    expire_month: '12',
-                    expire_year: '25',
-                    cvc: '152',
-                    zip_code: '07000', // not required
-                },
-                // buyer: '61307fb00d902e002c3ca187', // If using a saved buyer, set the _id from the IYZICO-Buyer Bucket
-                buyer: {
-                    id: 'BWDQ',
-                    name: 'Test',
-                    surname: 'User',
-                    email: 'test@gmail.com',
-                    identity_number: '231312123412',
-                    gsm_number: '333232432',
-                    ip: '192.168.1.1',
-                    city: 'Antalya',
-                    country: 'Turkey',
-                    registration_address: 'Arapsuyu mah. Konyaaltı',
-                    zipCode: '07000', // not required
-                },
-                // billingAddress: '612f70600d902e002c3ca0ca', // If using a saved billingAddress, set the _id from the IYZICO-Billing Address Bucket
-                billingAddress: {
-                    city: 'Istanbul',
-                    country: 'Turkey',
-                    address: 'Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1',
-                    contact_name: 'Test User',
-                    zip_code: '34000', // not required
-                },
-                // shippingAddress is required if there is a PHYSICAL type product in the basket. Not required (for VIRTUAL type products)
-                // shippingAddress: '61307f330d902e002c3ca183', // If using a saved shippingAddress, set the _id from the IYZICO-Shipping Address Bucket
-                shippingAddress: {
-                    contact_name: 'Test User',
-                    city: 'Antalya',
-                    country: 'Turkey',
-                    address: 'Arapsuyu mah. Konyaaltı',
-                    zip_code: '07000', // not required
-                },
-                // basket: '61307ef20d902e002c3ca17f', // If using a saved basket, set the _id from the IYZICO-Basket Items Address Bucket
-                basket: {
-                    name: 'Basket',
-                    product: ['612f8cc00d902e002c3ca0e3', '612f92200d902e002c3ca0f1'], // In the array, add the _id of the product that is in the IYZICO-Product Bucket
-                },
-                paidPrice: 160,
-            },
-        };
-        
+            "locale":"TR", // not required (default TR),
+            "email": "testuser@gmail.com",
+            "card": {
+                "card_holder_name": "Test User",
+                "card_number": "5528790000000008",
+                "expire_month": "12",
+                "expire_year": "2030"
+            }
+        }
 NOTES * 
  - Do not change the filled environment variables
 /************************************************************************/
@@ -76,10 +87,10 @@ const Iyzipay = require("iyzipay");
 const SECRET_API_KEY = process.env.SECRET_API_KEY;
 const PAY_BUCKET_ID = process.env.PAY_BUCKET_ID;
 const BUYER_BUCKET_ID = process.env.BUYER_BUCKET_ID;
-const PAYMENT_CARD_BUCKET_ID = process.env.PAYMENT_CARD_BUCKET_ID;
 const BILLING_ADDRESS_BUCKET_ID = process.env.BILLING_ADDRESS_BUCKET_ID;
 const SHIPPING_ADDRESS_BUCKET_ID = process.env.SHIPPING_ADDRESS_BUCKET_ID;
 const BASKET_ITEMS_BUCKET_ID = process.env.BASKET_ITEMS_BUCKET_ID;
+const CARD_BUCKET_ID = process.env.CARD_BUCKET_ID;
 
 const IYZIPAY_API_KEY = process.env.IYZIPAY_API_KEY;
 const IYZIPAY_SECRET_KEY = process.env.IYZIPAY_SECRET_KEY;
@@ -127,11 +138,15 @@ export async function payment(req, res) {
                 } else {
                     let status = 200;
                     if (result.status == "failure") status = 400;
-
+                    let transactionsId = [];
+                    result.itemTransactions.forEach(transaction => {
+                        transactionsId.push(transaction.paymentTransactionId);
+                    });
                     await updatePayData(
                         payData,
                         result.status,
                         result.paymentId,
+                        transactionsId,
                         result.errorMessage
                     );
                     return res.status(status).send({ message: result });
@@ -158,11 +173,12 @@ export async function payment(req, res) {
     }
 }
 
-async function updatePayData(payData, status, paymentId, errorMessage) {
+async function updatePayData(payData, status, paymentId, itemTransactions, errorMessage) {
     Bucket.initialize({ apikey: SECRET_API_KEY });
 
     payData.status = status;
     payData.payment_id = paymentId;
+    payData.item_transactions = itemTransactions;
     payData.error_message = errorMessage;
 
     await Bucket.data
@@ -196,29 +212,37 @@ async function getBuyerData(buyer) {
     return buyerData;
 }
 
+async function getBuyerEmail(buyerId) {
+    Bucket.initialize({ apikey: SECRET_API_KEY });
+
+    const buyer = await Bucket.data.get(BUYER_BUCKET_ID, buyerId);
+
+    return buyer.email || undefined;
+}
+
 async function getPaymentCard(data, buyerData) {
     Bucket.initialize({ apikey: SECRET_API_KEY });
     let cardData;
-    
-    //createCard(data, buyerData) !TODO
-    
+
+    const buyerEmail = await getBuyerEmail(buyerData);
+
     if (data.paymentCard) {
         if (typeof data.paymentCard == "string") {
             cardData = data.paymentCard;
         } else {
-            const existingCard = await Bucket.data.getAll(PAYMENT_CARD_BUCKET_ID, {
+            const existingCard = await Bucket.data.getAll(CARD_BUCKET_ID, {
                 queryParams: {
                     filter: {
-                        card_number: data.paymentCard.card_number
+                        email: buyerData.email
                     }
                 }
             });
 
-            if (existingCard[0] && existingCard[0].card_number == data.paymentCard.card_number) {
+            if (existingCard[0]) {
                 cardData = existingCard[0]._id;
             } else {
-                cardData = await Bucket.data.insert(PAYMENT_CARD_BUCKET_ID, data.paymentCard);
-                cardData = cardData._id;
+                const result = await createCard(data.locale, buyerEmail, data.paymentCard);
+                if (result) cardData = result.cardId;
             }
         }
     }
@@ -226,25 +250,71 @@ async function getPaymentCard(data, buyerData) {
     return cardData;
 }
 
-// function createCard(data, buyerData) {
-//     iyzipay.card.create(
-//         {
-//             locale: data.locale || "TR",
-//             email: buyerData.email,
-//             card: {
-//                 cardAlias: `${buyerData.name} ${buyerData.surname} card`,
-//                 cardHolderName: data.paymentCard.card_holder_name,
-//                 cardNumber: data.paymentCard.card_number,
-//                 expireMonth: data.paymentCard.expire_month,
-//                 expireYear: data.paymentCard.expire_year
-//             }
-//         },
-//         function(err, result) {
-//             console.log(err, result);
-//             done();
-//         }
-//     );
-// }
+export async function createIyziCard(req, res) {
+    const { locale, email, card } = req.body;
+
+    Bucket.initialize({ apikey: SECRET_API_KEY });
+    const cardData = await Bucket.data.getAll(CARD_BUCKET_ID, {
+        queryParams: {
+            filter: {
+                email: email
+            }
+        }
+    });
+    if (cardData[0]) {
+        return res.status(400).send({ message: "Email must be unique" });
+    } else {
+        const result = await createCard(locale, email, card);
+
+        return res.status(result.status).send({ message: result.message });
+    }
+}
+
+async function createCard(locale, email, card) {
+    Bucket.initialize({ apikey: SECRET_API_KEY });
+    let cardResult;
+    let iyzicoPromise = new Promise(function(resolve, reject) {
+        iyzipay.card.create(
+            {
+                locale: locale || "TR",
+                email: email,
+                card: {
+                    cardAlias: `${card.card_holder_name} Card`,
+                    cardHolderName: card.card_holder_name,
+                    cardNumber: card.card_number,
+                    expireMonth: card.expire_month,
+                    expireYear: card.expire_year
+                }
+            },
+            function(err, result) {
+                if (result) resolve(result);
+                else reject(err);
+            }
+        );
+    });
+
+    const result = await iyzicoPromise.catch(err => {
+        return { status: 400, message: err };
+    });
+    if (result) {
+        let status = 200;
+        if (result.status == "failure") status = 400;
+        else {
+            let cardData = {
+                email: result.email,
+                card_user_key: result.cardUserKey,
+                card_token: result.cardToken,
+                card_alias: result.cardAlias
+            };
+            cardResult = await Bucket.data.insert(CARD_BUCKET_ID, cardData);
+        }
+        return {
+            status: status,
+            message: result,
+            cardId: cardResult ? cardResult._id : undefined
+        };
+    }
+}
 
 async function getBillingAddress(billingAddress) {
     Bucket.initialize({ apikey: SECRET_API_KEY });
@@ -345,6 +415,11 @@ async function getPaymentData(payId) {
     }
 
     payRequest = replaceObject(payRequest);
+
+    if (payRequest.paymentCard) {
+        delete payRequest.paymentCard.email;
+        delete payRequest.paymentCard.cardAlias;
+    }
 
     return payRequest;
 }
